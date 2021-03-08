1aec8ac1c9d95efbf2c80af9ca68d82e4e77132b
michzimm <michzimm@cisco.com>
2021-03-08 17:19:18 -0500

    fixed issue

--- cleanuphx.py
+++ cleanuphx.py
@@ -119,7 +119,7 @@ def set_vmedia_boot_policy(ucs_handle, sp_template_boot_policy_object, org_name)
     ucs_handle.add_mo(mo, True)
     ucs_handle.commit()
 
-    if sp_template_boot_policy_name in ("HyperFlex"):
+    if sp_template_boot_policy_name in ("HyperFlex", "HyperFlex-sd"):
         mo = LsbootUsbFlashStorageImage(parent_mo_or_dn="org-root/org-"+org_name+"/boot-policy-"+sp_template_boot_policy_name+"/storage/local-storage", order="3")
     elif sp_template_boot_policy_name in ("HyperFlex-m5", "HyperFlex-m2pch"):
         mo = LsbootEmbeddedLocalDiskImage(parent_mo_or_dn="org-root/org-"+org_name+"/boot-policy-"+sp_template_boot_policy_name+"/storage/local-storage", order="3")
